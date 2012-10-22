cdfa4f0d5c0abcefcf31a5ff8e8b3895dd8b8fa7
David Yeung <quxbaz@gmail.com>
2012-10-22 02:21:11 -0400

    Big update. Using wooden background texture.

--- static/js/app/app.js
+++ static/js/app/app.js
@@ -5,4 +5,16 @@ $(function() {
     if (path.length == 0) path = 'home';
     $('#nav a[href="' + path + '"]').addClass('current');
 
+    $('#pulldown').toggle(function() {
+        $('#footer').slideDown(0);
+        $(this).css({bottom: 140})
+            .find('.text').text('less');
+        $('i', this).removeClass('icon-arrow-up').addClass('icon-arrow-down');
+    }, function() {
+        $('#footer').slideUp(0);
+        $(this).css({bottom: 0})
+            .find('.text').text('more');
+        $('i', this).removeClass('icon-arrow-down').addClass('icon-arrow-up');
+    });
+
 });
