7bf49e0ea630ad28261a6a87f9c552d4595e74c7
David Yeung <quxbaz@gmail.com>
2012-10-27 00:48:30 -0400

    Footer is no longer fixed.

--- static/js/app/app.js
+++ static/js/app/app.js
@@ -5,17 +5,17 @@ $(function() {
     if (path.length == 0) path = 'home';
     $('#nav a[href="' + path + '"]').addClass('current');
 
-    $('#pulldown').toggle(function() {
-        $('#footer').slideDown(0);
-        $(this).css({bottom: 140})
-            .find('.text').text('less');
-        $('i', this).removeClass('icon-arrow-up').addClass('icon-arrow-down');
-    }, function() {
-        $('#footer').slideUp(0);
-        $(this).css({bottom: 0})
-            .find('.text').text('more');
-        $('i', this).removeClass('icon-arrow-down').addClass('icon-arrow-up');
-    });
+    // $('#pulldown').toggle(function() {
+    //     $('#footer').slideDown(0);
+    //     $(this).css({bottom: 140})
+    //         .find('.text').text('less');
+    //     $('i', this).removeClass('icon-arrow-up').addClass('icon-arrow-down');
+    // }, function() {
+    //     $('#footer').slideUp(0);
+    //     $(this).css({bottom: 0})
+    //         .find('.text').text('more');
+    //     $('i', this).removeClass('icon-arrow-down').addClass('icon-arrow-up');
+    // });
 
     $('#portfolio .info-tab').toggle(function() {
         $(this).siblings('.site-info').slideDown(300);
