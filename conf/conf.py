c8826747f9835fb9edaed2e3d1bfcc746dbce9d4
David Yeung <quxbaz@gmail.com>
2012-11-02 00:19:22 -0400

    Added conf files.

---
+++ conf/conf.py
@@ -0,0 +1,14 @@
+# Settings file
+
+DEBUG = True
+
+PROJECT_PATH = '/'
+
+STATIC_DIR = r'/static/'
+
+EMAIL_USERNAME = ''
+EMAIL_PASSWORD = ''
+EMAIL_DESTINATION = ''
+
+# This line must go at the end of this file.
+from conf_dev import *
